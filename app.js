0ad60448cf2fba10d8b30f381af16ecceed9e41a
hiro <hironobu.kamata@adglobe.co.jp>
2023-12-22 18:23:39 +0900

    バリデーション等の追加

--- app.js
+++ app.js
@@ -10,14 +10,69 @@ const server = app.listen(3001, function() {
     console.log("Node.js is listening to PORT:" + server.address().port)
 })
 
-/***  以下に処理を記述 ***/
-
-const prisma = new PrismaClient();
-
-// 表示
 app.get("/", function(req, res) {
     res.render("index", {})
 })
+
+/*** バリデーション ***/
+
+const createValidation = async (req, res, next) => {
+    if(!req.query.name || !req.query.email) {
+        return res.json({message: "必須エラーです。"})
+    }
+
+    const users = await prisma.user.findMany({
+        where: {
+            email: req.query.email
+        },
+    })
+
+    if(users.length != 0) {
+        return res.json({message: "既に登録済みのユーザーです。"})
+    }
+
+    next()
+}
+
+const updateValidation = async (req, res, next) => {
+    if(!req.query.id || !req.query.name || !req.query.email) {
+        return res.json({message: "必須エラーです。"})
+    }
+
+    const users = await prisma.user.findMany({
+        where: {
+            id: parseInt(req.query.id)
+        },
+    })
+
+    if(users.length == 0) {
+        return res.json({message: "存在しないユーザーです。"})
+    }
+
+    next()
+}
+
+const deleteValidation = async (req, res, next) => {
+    if(!req.query.id) {
+        return res.json({message: "IDは必須です。"})
+    }
+
+    const users = await prisma.user.findMany({
+        where: {
+            id: parseInt(req.query.id)
+        },
+    })
+
+    if(users.length == 0) {
+        return res.json({message: "存在しないユーザーです。"})
+    }
+
+    next()
+}
+
+/***  CRUD処理 ***/
+
+const prisma = new PrismaClient();
 
 // 取得処理
 app.get("/userList", async (req, res) => {
@@ -27,6 +82,11 @@ app.get("/userList", async (req, res) => {
 
 // 条件付き取得処理
 app.get("/user/", async (req, res) => {
+
+    if(!req.query.id) {
+        return res.json({message: "IDは必須です。"})
+    }
+
     const users = await prisma.user.findMany({
         where: {
             id: parseInt(req.query.id),
@@ -36,19 +96,21 @@ app.get("/user/", async (req, res) => {
 })
 
 // 追加処理
-app.post("/addUser", async (req, res) => {
+app.post("/addUser", createValidation, async (req, res) => {
+
     const result = await prisma.user.create({
         data: {
-            id: parseInt(req.query.id),
             name: req.query.name,
             email: req.query.email
         }
     })
+
     return res.json(result)
 })
 
 // 更新処理
-app.put("/updateUser", async (req, res) => {
+app.put("/updateUser", updateValidation, async (req, res) => {
+
     const result = await prisma.user.update({
         where: {
             id: parseInt(req.query.id),
@@ -62,7 +124,7 @@ app.put("/updateUser", async (req, res) => {
 })
 
 // 削除処理
-app.delete("/deleteUser", async (req, res) => {
+app.delete("/deleteUser", deleteValidation, async (req, res) => {
     const result = await prisma.user.deleteMany({
         where: {
             id: parseInt(req.query.id),
